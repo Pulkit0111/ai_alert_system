0e1e2ded493c2093d0c7f2d598b7d83cf9cf359a
Pulkit0111 <ptyagi1551@gmail.com>
2025-07-09 20:11:48 +0530

    added the option to ask from user for the location

--- data_sources/weather_api.py
+++ data_sources/weather_api.py
@@ -1,23 +1,17 @@
-# data_sources/weather_api.py
-
 import requests
-from config import weather_api_key, LOCATION
+from config import weather_api_key
 
-def fetch_weather_alerts():
+def fetch_weather_alerts(location: str):
     url = (
         f"https://api.weatherapi.com/v1/forecast.json"
-        f"?key={weather_api_key}&q={LOCATION}&days=1&alerts=yes"
+        f"?key={weather_api_key}&q={location}&days=1&alerts=yes"
     )
 
     try:
         response = requests.get(url)
         data = response.json()
-
         alerts = data.get("alerts", {}).get("alert", [])
-        if alerts:
-            return alerts
-        else:
-            return []
+        return alerts
     except Exception as e:
-        print("❌ Error fetching weather data:", e)
+        print("❌ Error fetching WeatherAPI alerts:", e)
         return []
